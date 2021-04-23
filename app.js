e1f54c4676ff9f4bd13485c69d75cf51e270deb6
Laurentiu Dodoloi <laurentiu@cobrowser.net>
2021-04-23 14:30:36 +0300

    feat: setup project

--- app.js
+++ app.js
@@ -1,41 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const PORT = process.env.PORT || 8080;
 
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get("/", function(req, res){
+  res.render("index", { testvar: 'WORKS FINE.' });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.post('/search', (req, res) => {
+  const search = req.body.search;
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.render("statistics", { search });
 });
 
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server listening at localhost:${PORT}`)
+});
